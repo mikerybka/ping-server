d9af82226dd1933e0112173a2279326bfeee6d0f
Mike Rybka <merybka@gmail.com>
2025-01-18 12:36:19 -0500

    Allow custom message

--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "ok")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-
 	// Get the port from the environment variable, default to 3000
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+
+	msg := os.Getenv("MESSAGE")
+	if msg == "" {
+		msg = "ok"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, msg)
+	})
 
 	// Start the server on the specified port
 	fmt.Printf("Server is running on http://localhost:%s\n", port)
